f4e2b5952082285a6e7496eab3a02f060cf77641
alaahatem <alaa.kordy@statsbomb.com>
2024-11-11 03:51:59 +0200

    fix: rerenders in useapi hooks

--- src/app/hooks/useApi.ts
+++ src/app/hooks/useApi.ts
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback } from 'react';
+import { useState, useEffect } from 'react';
 import axios, { AxiosError } from 'axios';
 import { SERVICE_HOST } from '@/lib/constants';
 
@@ -13,24 +13,24 @@ export function useApi<T>({ endpoint, params = {}, initialData }: ApiOptions<T>)
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState<Error | null>(null);
 
-  const fetchData = useCallback(async () => {
-    setIsLoading(true);
-    try {
-      const response = await axios.get<T>(`https://${SERVICE_HOST}${endpoint}`, { params });
-      setData(response.data);
-      setError(null);
-    } catch (err) {
-      const error = err as Error | AxiosError;
-      setError(error instanceof Error ? error : new Error('An error occurred'));
-      console.error(`Failed to fetch data from ${endpoint}`, error);
-    } finally {
-      setIsLoading(false);
-    }
-  }, [endpoint, params]);
-
   useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      try {
+        const response = await axios.get<T>(`https://${SERVICE_HOST}${endpoint}`, { params });
+        setData(response.data);
+        setError(null);
+      } catch (err) {
+        const error = err as Error | AxiosError;
+        setError(error instanceof Error ? error : new Error('An error occurred'));
+        console.error(`Failed to fetch data from ${endpoint}`, error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
     fetchData();
-  }, [fetchData]);
+  }, [endpoint, JSON.stringify(params)]);
 
   return { data, isLoading, error };
 }
